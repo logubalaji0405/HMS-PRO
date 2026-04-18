5dc16bae232b111bde6aecb7a80a5a9329814252
012balaji <144882695+012balaji@users.noreply.github.com>
2026-04-18 12:31:07 +0530

    switch database to postgres

--- hospital_project/settings.py
+++ hospital_project/settings.py
@@ -2,13 +2,14 @@
 
 from pathlib import Path
 import os
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-w=27^x_&og^&rhkbaxyeiapv@s)6h%ykz!qquo-+&k(2*ep%ic'
 DEBUG = True
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ["your-project.vercel.app", ".vercel.app", "127.0.0.1", "localhost"]
+CSRF_TRUSTED_ORIGINS = ["https://your-project.vercel.app"]
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -50,10 +51,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'hospital_project.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
